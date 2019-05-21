61641d6e55ded16040da8ed3bba37daf03bebc9e
GiffordWebite <49037457+GiffordWebite@users.noreply.github.com>
2019-05-21 08:01:20 -0600

    Update konami.js

--- docs/js/konami.js
+++ docs/js/konami.js
@@ -38,7 +38,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 function activateCheats() {
-  document.body.style.backgroundImage = "url('cheatBackground.png')";
+  document.body.style.backgroundImage = "url('CheatBackground.png')";
 
   alert("Mrs. Gifford Is Amazing");
 }
